c83af80e73e9be782323501d9673c80105038ebd
KevinOrellana26 <kevinorellana202@gmail.com>
2023-08-07 10:35:42 +0200

    update

--- main.tf
+++ main.tf
@@ -33,33 +33,92 @@ resource "kubernetes_namespace" "default" {
   }
 }
 
-resource "kubernetes_manifest" "angular-pvc" {
-  depends_on = [
-    kubernetes_namespace.default
-  ]
-  manifest = yamldecode(templatefile(
-    "${path.module}/manifests/angular-pvc.tpl.yaml",
-    {
-      "namespace" = var.namespace
-    }
-  ))
-}
+# resource "kubernetes_manifest" "angular-pvc" {
+#   depends_on = [
+#     kubernetes_namespace.default
+#   ]
+#   manifest = yamldecode(templatefile(
+#     "${path.module}/manifests/angular-pvc.tpl.yaml",
+#     {
+#       "namespace" = var.namespace
+#     }
+#   ))
+# }
 
-resource "kubernetes_stateful_set" "angular" {
-  depends_on = [
+# resource "kubernetes_stateful_set" "angular" {
+#   depends_on = [
+#     kubernetes_namespace.default
+#   ]
+#   metadata {
+#     name      = "angular"
+#     namespace = var.namespace
+#   }
+#   spec {
+#     replicas = 1
+#     selector {
+#       match_labels = {
+#         app = "angular"
+#       }
+#     }
+#     template {
+#       metadata {
+#         labels = {
+#           app = "angular"
+#         }
+#       }
+#       spec {
+#         volume {
+#           name = "angular"
+#           persistent_volume_claim {
+#             claim_name = "angular-pvc"
+#           }
+#         }
+#         container {
+#           name  = "angular"
+#           image = "kevinorellana/angular:v1"
+#           port {
+#             container_port = 4500
+#           }
+#           resources {
+#             limits = {
+#               memory = "5Gi"
+#               cpu    = "1000m"
+#             }
+#             requests = {
+#               memory = "2Gi"
+#               cpu    = "500m"
+#             }
+#           }
+#           volume_mount {
+#             name       = "angular"
+#             mount_path = "/hello-world"
+#           }
+#           image_pull_policy = "Always"
+#         }
+#       }
+#     }
+#     service_name = "angular"
+#     update_strategy {
+#       type = "RollingUpdate"
+#     }
+#   }
+# }
+
+resource "kubernetes_deployment" "angular_deploy" {
+  depends_on = [ 
     kubernetes_namespace.default
   ]
   metadata {
-    name      = "angular"
+    name = "angular"
     namespace = var.namespace
   }
   spec {
-    replicas = 1
     selector {
       match_labels = {
         app = "angular"
       }
     }
+    replicas = 1
     template {
       metadata {
         labels = {
@@ -67,39 +126,15 @@ resource "kubernetes_stateful_set" "angular" {
         }
       }
       spec {
-        volume {
-          name = "angular"
-          persistent_volume_claim {
-            claim_name = "angular-pvc"
-          }
-        }
         container {
-          name  = "angular"
+          name = "angular"
           image = "kevinorellana/angular:v1"
           port {
             container_port = 4500
           }
-          resources {
-            limits = {
-              memory = "5Gi"
-              cpu    = "1000m"
-            }
-            requests = {
-              memory = "2Gi"
-              cpu    = "500m"
-            }
-          }
-          volume_mount {
-            name       = "angular"
-            mount_path = "/hello-world"
-          }
           image_pull_policy = "Always"
         }
       }
-    }
-    service_name = "angular"
-    update_strategy {
-      type = "RollingUpdate"
     }
   }
 }
